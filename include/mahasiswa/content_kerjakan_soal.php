fee65b63f4c66062bab501ff2f7bd671e2d1f9a5
jhonarendra <jhonarendra@gmail.com>
2018-10-22 17:51:17 -0700

    mahasiswa input jawaban

--- include/mahasiswa/content_kerjakan_soal.php
+++ include/mahasiswa/content_kerjakan_soal.php
@@ -1,38 +1,44 @@
 <?php
-$id_mhs = $_SESSION['id_mhs'];
-echo $id_mhs;
-echo $nama_ujian;
+	$id_mhs = $_SESSION['id_mhs'];
+	$nama_ujian = $_GET['kerjakan'];
+
+	if (isset($_POST['submit'])) {
+		$sum_nilai = 0;
+		$id_ujian = $_POST['id_ujian'];
+		$jumlah_soal = 0;
+		for ($i=0; $i<100 ; $i++) { 
+			if(isset($_POST['jawaban_mhs'.$i])){
+				$jawaban = $_POST['jawaban_mhs'.$i];
+				$kunci_jawaban = $_POST['kunci_jawaban'.$i];
+				similar_text($jawaban, $kunci_jawaban, $nilai_similarity);
+				$sum_nilai = $nilai_similarity+$sum_nilai;
+				$insert_soal = mysqli_query($conn, "INSERT INTO tb_jawaban_mhs VALUES(NULL, $i, $id_mhs, '$jawaban', $nilai_similarity)");
+				$jumlah_soal++;
+			}
+		}
+		$tot_nilai = $sum_nilai/$jumlah_soal;
+		$insert_nilai = mysqli_query($conn, "INSERT INTO tb_nilai_mhs VALUES (NULL, $id_ujian, $id_mhs, $tot_nilai)");
+	}
 ?>
 <div class="card mb-3">
 	<div class="card-header">
-		<i class="fas fa-table"></i>Dashboard
+		<i class="fas fa-table"></i> Dashboard
 	</div>
 	<div class="card-body">
-	<form id="form" action="" method="POST">
-			<div class="row">
-				<div class="col-sm-1">
-					<p class="h1">
-						1
-					</p>
-				</div>
-				<div class="col-sm-5">
-					<label for="soal1">Soal</label>
-					<textarea class="form-control" name="soal<?php echo $nomor_soal;?>" rows="3"></textarea>
-				</div>
-				<div class="col-sm-6">
-					<label for="jawaban1">Jawaban</label>
-					<textarea class="form-control" name="jawaban<?php echo $nomor_soal;?>" rows="3"></textarea>
-				</div>   
-			</div>
-
-			<div class="form-group" style="position: fixed;z-index: 9999;right: 20px;bottom:20x">
-				<a class="btn btn-primary" id="btn-tambah-soal" href="javascript:void(o)">
-					<i class="fas fa-plus"></i> Tambah Soal
-				</a>  
-				<button class="btn btn-success" type="submit" name="submit" id="btn-tambah-soal">
-					Submit
-				</button>           
-			</div>
+		<form id="form" action="" method="POST">
+		<?php
+			$semua_soal = mysqli_query($conn, "SELECT * FROM tb_soal INNER JOIN tb_ujian ON tb_soal.`id_ujian`=tb_ujian.`id_ujian` WHERE nama_ujian='$nama_ujian' ORDER BY nomor_soal");
+			foreach ($semua_soal as $soal) {
+		?>
+			<p><?php echo $soal['nomor_soal'].". ".$soal['soal']?></p>
+			<input type="text" name="jawaban_mhs<?php echo $soal['id_soal']?>">
+			<input type="hidden" name="kunci_jawaban<?php echo $soal['id_soal']?>" value="<?php echo $soal['kunci_jawaban']?>">
+			<br /><br />
+		<?php
+			}
+		?>
+			<input type="hidden" name="id_ujian" value="<?php echo $soal['id_ujian'] ?>">
+			<input type="submit" name="submit" value="kirim">
 		</form>
 	</div>
 	<div class="card-footer small text-muted">Updated yesterday at 11:59 PM</div>
