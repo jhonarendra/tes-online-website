99063f51360494d6830a9eeee74a00a87c2bbfd7
unknown <devajayantha870@gmail.com>
2018-10-29 19:30:38 +0800

    edit duluan

--- include/mahasiswa/sidebar_mhs.php
+++ include/mahasiswa/sidebar_mhs.php
@@ -1,14 +1,14 @@
 <ul class="sidebar navbar-nav">
-    <!-- <li class="nav-item active">
+    <li class="nav-item active">
         <a class="nav-link" href="mahasiswa">
         <i class="fas fa-fw fa-home"></i>
         <span>Daftar Ujian</span>
         </a>
     </li> 
-    <li class="nav-item active">
+    <!-- <li class="nav-item active">
         <a class="nav-link" href="?page=nilai">
         <i class="fas fa-chalkboard-teacher"></i>
         <span>Nilai</span>
         </a>
-    </li>  -->                     
+    </li>                    -->
 </ul>
